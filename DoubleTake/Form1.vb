7f3689ec77c455eba754c4b9a282c1ef07c28a24
Tyler De Witt <tyler.dewitt1785@gmail.com>
2019-10-24 09:14:15 -0500

    Add files via upload

---
+++ DoubleTake/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private turnCounter As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
