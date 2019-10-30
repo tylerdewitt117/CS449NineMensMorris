d11a764836781b4ac5ee1adf9e0feb7a028f7932
Tyler De Witt <tyler.dewitt1785@gmail.com>
2019-10-30 10:00:48 -0500

    Add files via upload
    added logic to determine if a mill has been formed (not connected)

---
+++ DoubleTake/Mills.vb
@@ -0,0 +1,201 @@
+﻿Public Class Mills
+
+    Public Function isMill() As Boolean
+        'determine if white pieces have made a mill
+        If Buttons.Button1.BackColor = Color.White AndAlso Buttons.Button2.BackColor = Color.White AndAlso Buttons.Button3.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button4.BackColor = Color.White AndAlso Buttons.Button5.BackColor = Color.White AndAlso Buttons.Button6.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button7.BackColor = Color.White AndAlso Buttons.Button8.BackColor = Color.White AndAlso Buttons.Button9.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button10.BackColor = Color.White AndAlso Buttons.Button11.BackColor = Color.White AndAlso Buttons.Button12.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button13.BackColor = Color.White AndAlso Buttons.Button14.BackColor = Color.White AndAlso Buttons.Button15.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button16.BackColor = Color.White AndAlso Buttons.Button17.BackColor = Color.White AndAlso Buttons.Button18.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button19.BackColor = Color.White AndAlso Buttons.Button20.BackColor = Color.White AndAlso Buttons.Button21.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button22.BackColor = Color.White AndAlso Buttons.Button23.BackColor = Color.White AndAlso Buttons.Button24.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button1.BackColor = Color.White AndAlso Buttons.Button10.BackColor = Color.White AndAlso Buttons.Button22.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button4.BackColor = Color.White AndAlso Buttons.Button11.BackColor = Color.White AndAlso Buttons.Button19.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button7.BackColor = Color.White AndAlso Buttons.Button12.BackColor = Color.White AndAlso Buttons.Button16.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button2.BackColor = Color.White AndAlso Buttons.Button5.BackColor = Color.White AndAlso Buttons.Button8.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button17.BackColor = Color.White AndAlso Buttons.Button20.BackColor = Color.White AndAlso Buttons.Button23.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button9.BackColor = Color.White AndAlso Buttons.Button13.BackColor = Color.White AndAlso Buttons.Button18.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button6.BackColor = Color.White AndAlso Buttons.Button14.BackColor = Color.White AndAlso Buttons.Button21.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button3.BackColor = Color.White AndAlso Buttons.Button15.BackColor = Color.White AndAlso Buttons.Button24.BackColor = Color.White Then
+            Return True
+        Else
+            Return False
+        End If
+
+
+        'determine if black pieces have made a mill
+        If Buttons.Button1.BackColor = Color.Black AndAlso Buttons.Button2.BackColor = Color.Black AndAlso Buttons.Button3.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button4.BackColor = Color.Black AndAlso Buttons.Button5.BackColor = Color.Black AndAlso Buttons.Button6.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button7.BackColor = Color.Black AndAlso Buttons.Button8.BackColor = Color.Black AndAlso Buttons.Button9.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button10.BackColor = Color.Black AndAlso Buttons.Button11.BackColor = Color.Black AndAlso Buttons.Button12.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button13.BackColor = Color.Black AndAlso Buttons.Button14.BackColor = Color.Black AndAlso Buttons.Button15.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button16.BackColor = Color.Black AndAlso Buttons.Button17.BackColor = Color.Black AndAlso Buttons.Button18.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button19.BackColor = Color.Black AndAlso Buttons.Button20.BackColor = Color.Black AndAlso Buttons.Button21.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button22.BackColor = Color.Black AndAlso Buttons.Button23.BackColor = Color.Black AndAlso Buttons.Button24.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button1.BackColor = Color.Black AndAlso Buttons.Button10.BackColor = Color.Black AndAlso Buttons.Button22.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button4.BackColor = Color.Black AndAlso Buttons.Button11.BackColor = Color.Black AndAlso Buttons.Button19.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button7.BackColor = Color.Black AndAlso Buttons.Button12.BackColor = Color.Black AndAlso Buttons.Button16.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button2.BackColor = Color.Black AndAlso Buttons.Button5.BackColor = Color.Black AndAlso Buttons.Button8.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button17.BackColor = Color.Black AndAlso Buttons.Button20.BackColor = Color.Black AndAlso Buttons.Button23.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button9.BackColor = Color.Black AndAlso Buttons.Button13.BackColor = Color.Black AndAlso Buttons.Button18.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button6.BackColor = Color.Black AndAlso Buttons.Button14.BackColor = Color.Black AndAlso Buttons.Button21.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+
+        If Buttons.Button3.BackColor = Color.Black AndAlso Buttons.Button15.BackColor = Color.Black AndAlso Buttons.Button24.BackColor = Color.Black Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+End Class
